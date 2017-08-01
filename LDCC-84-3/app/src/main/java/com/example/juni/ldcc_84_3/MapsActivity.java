16c79c32ade64ce78d2fb8e3e44ebbcbef5be1c8
Lee-jun-hee <wns8432@naver.com>
2017-08-01 15:24:16 +0900

    안정성 강화

--- LDCC-84-3/app/src/main/java/com/example/juni/ldcc_84_3/MapsActivity.java
+++ LDCC-84-3/app/src/main/java/com/example/juni/ldcc_84_3/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -19,6 +20,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ArrayList<String> sevenArr;
     ArrayList<String> xposArr;
     ArrayList<String> yposArr;
+    String curx;
+    String cury;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +31,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         sevenArr = intent.getStringArrayListExtra("sevenarr");
         xposArr = intent.getStringArrayListExtra("xposarr");
         yposArr = intent.getStringArrayListExtra("yposarr");
+        curx = intent.getStringExtra("curx");
+        cury = intent.getStringExtra("cury");
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -49,12 +53,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
         for(int i =0; i < sevenArr.size(); i++){
             LatLng position = new LatLng(Float.valueOf(xposArr.get(i)), Float.valueOf(yposArr.get(i)));
-            mMap.addMarker(new MarkerOptions().position(position).title(getResources().getStringArray(R.array.seven_name)[Integer.valueOf(sevenArr.get(i)) - 1]));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+            mMap.addMarker(new MarkerOptions().position(position).title(getResources().getStringArray(R.array.seven_name)[Integer.valueOf(sevenArr.get(i)) - 1])
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         }
+        LatLng position = new LatLng(Float.valueOf(curx), Float.valueOf(cury));
+        mMap.addMarker(new MarkerOptions().
+                position(position).
+                title(getResources().getString(R.string.currentpos)).
+                icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
     }
 }
